b8199a4cb4736e3f77423741a92783f777cd4818
tuminhdiep <tuminh2307@gmail.com>
2025-04-24 18:11:37 -0400

    create the frame of game

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
 
+import javax.swing.JFrame;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        //size of the frame    
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        //Create the frame of the game
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
